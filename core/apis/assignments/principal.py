aba2bd6d1492a9668f25f22f3b4731e5cea26623
Kavyesh Vohra <47300062+kavyeshvohra@users.noreply.github.com>
2024-08-01 20:57:06 +0000

    Final Commit

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -3,41 +3,34 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
-from core.models.teachers import Teacher
-
-from .schema import AssignmentSchema, AssignmentGradeSchema, TeacherSchema
+from sqlalchemy import or_
+from .schema import AssignmentSchema, AssignmentGradeSchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
 @principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
-def get_assignments(p):
-    """Lists all submitted and graded assignments"""
-    all_submitted_graded_assignments = Assignment.get_all_submitted_and_graded_assignments()
-    all_submitted_graded_assignments_dump = AssignmentSchema().dump(all_submitted_graded_assignments, many=True)
-    return APIResponse.respond(data=all_submitted_graded_assignments_dump)
+def list_submitted_and_graded_assignments(p):
+    """Returns list of assignments"""
+    principal_assignments = Assignment.filter(or_(Assignment.state == AssignmentStateEnum.GRADED,
+                                                  Assignment.state == AssignmentStateEnum.SUBMITTED)).all()
 
+    principal_assignments_dump = AssignmentSchema().dump(principal_assignments, many=True)
+    return APIResponse.respond(data=principal_assignments_dump)
 
-@principal_assignments_resources.route('/teachers', methods=['GET'], strict_slashes=False)
-@decorators.authenticate_principal
-def get_all_teachers(p):
-    """Lists all the teachers"""
-    all_teachers = Teacher.get_all_teachers()
-    all_teachers_dump = TeacherSchema().dump(all_teachers, many=True)
-    return APIResponse.respond(data=all_teachers_dump)
 
 @principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
-def grade_or_regrade_assignments(p, incoming_payload):
-    """Grades or regrades an assignment by the principal"""
-    grade_or_regrade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
-    graded_or_regraded_assignment = Assignment.mark_grade(
-        _id=grade_or_regrade_assignment_payload.id,
-        grade=grade_or_regrade_assignment_payload.grade,
-        auth_principal=p,
-        isTeacher=False
-    )
+def grade_assignment_by_principal(p, incoming_payload):
+    """Grade or re-grade an assignment"""
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
 
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    print(graded_assignment,end=" ")
     db.session.commit()
-    graded_or_regraded_assignment_dump = AssignmentSchema().dump(graded_or_regraded_assignment)
-    return APIResponse.respond(data=graded_or_regraded_assignment_dump)
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
